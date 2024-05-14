8b7681e5a6167d56c44a6983a67ef5d28a37c6b2
Moein Tavakoli <mtawakoli2283@gmail.com>
2024-05-14 13:41:22 +0330

    [ Fix ] update time , name tar file

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
       local_path_dir = sys.argv[1]
       if os.path.exists(local_path_dir):          
          print(f'local_path_dir = {local_path_dir}')
-         dir_to_tar(local_path_dir, f'{time_generator()}.tar.gz')
-         print(f'backup succesfuly as {time_generator()}.tar.gz file ...')
+         date_now = time_generator()
+         dir_to_tar(local_path_dir, f'{date_now}.tar.gz')
+         print(f'backup succesfuly as {date_now}.tar.gz file ...')
       else:
          raise Exception("No such file or directory !")
     else:
