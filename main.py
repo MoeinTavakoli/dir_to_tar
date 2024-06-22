6dbda36296c490ec882f688ef57bd1d388b23bd1
Moein Tavakoli <mtawakoli2283@gmail.com>
2024-06-22 15:06:57 +0330

    [ Feature ] Add Arg parse to add flags

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import sys
 import os
+import argparse
 from compress import dir_to_tar , time_generator
 
 if __name__ == '__main__':
+       
+    parser = argparse.ArgumentParser(description='Backup directory to a tar.gz file.')
+    parser.add_argument('-d', '--directory', required=True, help='Directory to be backed up')
+    args = parser.parse_args()
 
-    if len(sys.argv) > 1 : 
-      local_path_dir = sys.argv[1]
-      if os.path.exists(local_path_dir):          
-         print(f'local_path_dir = {local_path_dir}')
-         date_now = time_generator()
-         dir_to_tar(local_path_dir, f'{date_now}.tar.gz')
-         print(f'backup succesfuly as {date_now}.tar.gz file ...')
-      else:
-         raise Exception("No such file or directory !")
+    local_path_dir = args.directory
+    if os.path.exists(local_path_dir):
+        print(f'local_path_dir = {local_path_dir}')
+        date_now = time_generator()
+        dir_to_tar(local_path_dir, f'{date_now}.tar.gz')
+        print(f'backup successfully as {date_now}.tar.gz file ...')
     else:
-     raise Exception("argument is not define ! ")
+        raise Exception("No such file or directory!")
